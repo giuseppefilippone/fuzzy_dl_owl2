23f5932bbed09560559062d55cd98334bd0bcf91
Giuseppe Filippone <filipponegiuseppe94@gmail.com>
2025-06-06 14:36:33 +0200

    Update read the docs

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fuzzy Description Language Reasonear and Fuzzy OWL 2 ontology handler'
+copyright = '2025, Giuseppe Filippone'
+author = 'Giuseppe Filippone'
+release = 'v1.0.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Optional: if you use numpy-style or Google-style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# -- Options for LaTeX math rendering ---------------------------------
+
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,27 @@
+.. Fuzzy Description Language Reasonear and Fuzzy OWL 2 ontology handler documentation master file, created by
+   sphinx-quickstart on Fri Jun  6 14:31:01 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Fuzzy Description Language Reasonear and Fuzzy OWL 2 ontology handler documentation
+===================================================================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. toctree::
+   :maxdepth: 5
+   :caption: Contents:
+
+.. automodule:: fuzzy_dl_owl2
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
