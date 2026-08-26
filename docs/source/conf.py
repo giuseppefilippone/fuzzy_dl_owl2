d73ea66f54e35714c710be07474bb739040e384f
Giuseppe Filippone <filipponegiuseppe94@gmail.com>
2026-08-26 12:46:04 +0200

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,10 +15,10 @@ project = "Fuzzy DL OWL 2"
 copyright = "2025, Giuseppe Filippone"
 author = "Giuseppe Filippone"
 # The short X.Y version
-version = "1.0.26"
+version = "1.0.27"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.26"
+release = "1.0.27"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
